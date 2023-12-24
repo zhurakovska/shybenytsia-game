e6e834db2b6e0dee9415abecd333751d194b91a9
zhurakovska <n.sinawka@gmail.com>
2023-12-24 13:14:11 +0200

    added code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ let words = ["dog", "monkey", "amazing", "cat", "land"];
 
 let word = words[Math.floor(Math.random() * words.length)];
 console.log(word);
+
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+
+let remainingLetters = word.length;
+
+while (remainingLetters > 0) {
+  alert(answerArray.join(" "));
+
+  let guess = prompt("Guess a letter , or click Cancel to stop playing");
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    alert("Please enter a single letter");
+  } else {
+    for (let j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
+    }
+  }
+}
+
+alert(answerArray.join(" "));
+alert("Good job! The answer was" + word);
